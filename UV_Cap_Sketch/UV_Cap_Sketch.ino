65f316ae113073e84b9c6a54872458b784e945d3
ziontrane <lukas.heitkamp@informatik.hs-fulda.de>
2022-12-15 15:31:51 +0100

    UV-Sensor Test

--- UV_Cap_Sketch/UV_Cap_Sketch.ino
+++ UV_Cap_Sketch/UV_Cap_Sketch.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SparkFun_VEML6075_Arduino_Library.h>
 
+VEML6075 uv; // Create a VEML6075 object
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  // the VEML6075's begin function can take no parameters
+  // It will return true on success or false on failure to communicate
+  if (uv.begin() == false)
+  {
+    Serial.println("Unable to communicate with VEML6075.");
+    while (1)
+      ;
+  }
+  Serial.println("UVA, UVB, UV Index");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // Use the uva, uvb, and index functions to read calibrated UVA and UVB values and a
+  // calculated UV index value between 0-11.
+  Serial.println(String(uv.uva()) + ", " + String(uv.uvb()) + ", " + String(uv.index()));
+  delay(250);
 }
